73de189acdaee30e12190027025f9e8567eb892e
Iliya <razorilich@gmail.com>
2020-03-12 03:15:42 +0300

    middleware

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,92 +1,94 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const objectId = require("mongodb").ObjectID;
 const Card = require("../models/card");
 
-module.exports.get = (req, res) => {
-  id = req.params.id
+module.exports.get = async (req, res) => {
   try {
-    if (!id) {
-      Card.find({})
-        .then(cards => res.status(200).send({ data: cards }))
-        .catch(err => res.status(500).send({ message: err.message }));
-    } else {
-      Card.findById(id)
-        .then((err, cards) => {
-          if (err) {
-            res.status(404).send({ message: err.message });
-          }
-          res.status(200).send({ data: cards });
-        })
-        .catch(err => res.status(500).send({ message: err.message }));
-    }
+    const cards = await Card.find();
+    await res.json(cards);
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    await res.status(500).json({ message: err.message });
   }
 };
-module.exports.post = (req, res) => {
-  const name = req.body.name;
-  const link = req.body.link;
-  const likes = req.body.likes;
+
+module.exports.post = async (req, res) => {
+  const card = new Card({
+    name: req.body.name,
+    link: req.body.link,
+    // eslint-disable-next-line no-underscore-dangle
+    owner: req.user._id,
+    likes: req.body.likes
+  });
+
+  try {
+    const newCard = await card.save();
+    await res.status(201).json(newCard);
+  } catch (err) {
+    await res.status(400).json({ message: err.message });
+  }
+};
+// module.exports.like = (req, res) => {
+//   const cardId = req.params.id;
+//   const userId = req.user._id;
+//
+//   try {
+//     Card.findByIdAndUpdate(
+//       cardId,
+//       {
+//         $addToSet: { likes: userId }
+//       },
+//       { new: true }
+//     )
+//       .then(card => res.status(200).send({ data: card }))
+//       .catch(err => res.status(404).send({ message: err.message }));
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// };
+//
+module.exports.like = async (req, res) => {
   const userId = req.user._id;
-
+  await res.card.likes.addToSet(userId);
   try {
-    Card.create({ name, link, owner: userId, likes })
-      .then(card => res.status(201).send({ data: card }))
-      .catch(err => res.status(400).send({ message: err.message }));
-  } catch (err) {
-    res.status(400).send({ mesage: err.message });
+    const updatedCard = await res.card.save();
+    res.json(updatedCard);
+  } catch {
+    res.status(400).json({ message: err.message });
   }
 };
-module.exports.delete = (req, res) => {
-  try {
-    Card.findByIdAndDelete(req.params.id)
-      .then(card => {
-        res.status(200).send({ data: card });
-      })
-      .catch(err => res.status(404).send({ message: err.message }));
-  } catch (err) {
-    res.status(400).send({ message: err.message });
-  }
-};
+// module.exports.delete = (req, res) => {
+//   try {
+//     Card.findByIdAndDelete(req.params.id)
+//       .then(card => {
+//         res.status(200).send({ data: card });
+//       })
+//       .catch(err => res.status(404).send({ message: err.message }));
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// };
+//
 
-module.exports.like = (req, res) => {
-  const cardId = req.params.id;
-  const userId = req.user._id;
-
-  try {
-    Card.findByIdAndUpdate(
-      cardId,
-      {
-        $addToSet: { likes: userId }
-      },
-      { new: true }
-    )
-      .then(card => res.status(200).send({ data: card }))
-      .catch(err => res.status(404).send({ message: err.message }));
-  } catch (err) {
-    res.status(400).send({ message: err.message });
-  }
-};
-
-module.exports.unlike = (req, res) => {
-  const cardId = req.params.id;
-  const userId = req.user._id;
-
-  try {
-    Card.findByIdAndUpdate(
-      cardId,
-      {
-        $pull: { likes: userId }
-      },
-      { new: true }
-    )
-      .then((error, res) => {
-        res.status(401).send({ data: error });
-      })
-      .then(card => {
-        res.status(200).send({ data: card });
-      })
-      .catch(err => res.status(404).send({ message: err.message }));
-  } catch (err) {
-    res.status(400).send({ message: err.message });
-  }
-};
+// module.exports.unlike = (req, res) => {
+//   const cardId = req.params.id;
+//   const userId = req.user._id;
+//
+//   try {
+//     Card.findByIdAndUpdate(
+//       cardId,
+//       {
+//         $pull: { likes: userId }
+//       },
+//       { new: true }
+//     )
+//       .then((error, res) => {
+//         res.status(401).send({ data: error });
+//       })
+//       .then(card => {
+//         res.status(200).send({ data: card });
+//       })
+//       .catch(err => res.status(404).send({ message: err.message }));
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// };
--- models/card.js
+++ models/card.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    required: true
   },
   link: {
     type: String,
     validate: {
       validator(link) {
         return validator.isURL(link);
-      },
+      }
     },
-    required: true,
+    required: true
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
+    ref: "user",
+    required: true
   },
   likes: [{ type: mongoose.Schema.Types.ObjectId, default: [] }],
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('card', cardSchema);
+module.exports = mongoose.model("card", cardSchema);
